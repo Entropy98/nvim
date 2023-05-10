8f3d87251467ffda25274b7311dca59e7b2b8418
Entropy98 <harperweigle@gmail.com>
2023-05-10 11:16:15 -0700

    fixed space e conflict

--- lua/user/coc-clangd.lua
+++ lua/user/coc-clangd.lua
@@ -178,7 +178,8 @@ function M.config()
   -- Show all diagnostics
   keyset("n", "<space>a", ":<C-u>CocList diagnostics<cr>", opts)
   -- Manage extensions
-  keyset("n", "<space>e", ":<C-u>CocList extensions<cr>", opts)
+  -- Conflicts with file tree
+  -- keyset("n", "<space>e", ":<C-u>CocList extensions<cr>", opts)
   -- Show commands
   keyset("n", "<space>c", ":<C-u>CocList commands<cr>", opts)
   -- Find symbol of current document
